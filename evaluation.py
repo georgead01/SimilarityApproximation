8a450b1fcf580ad9c9a9034ad1e9eb079d7c4df1
George Abu Daoud <abudauodg@gmail.com>
2025-05-01 18:11:26 -0400

    added precision@k, AP@k and MAP@k testing and implementation

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # methods
 def reciprocal_rank(ranking, relevant_docs):
     '''
@@ -18,7 +20,6 @@ def reciprocal_rank(ranking, relevant_docs):
         
     return 0
 
-# methods
 def reciprocal_rank_k(ranking, relevant_docs, k):
     '''
     Computes the Reciprocal Rank @ k for a ranking given the indices relevant documents.
@@ -52,6 +53,95 @@ def mean_reciprocal_rank_k(rankings, relevant_docs, k):
 
     mrr /= num_queries
     return mrr
+
+def precision_k(ranking, relevant_docs, k):
+    '''
+    Computes the Precision @ k for a ranking given the indices relevant documents.
+
+    Attributes:
+    - ranking: a list of document indices (integers) order in decreasing predicted relevance, must not be empty
+    - relevant_docs: a list of document indices (integers) that are relevant, must not be empty
+
+    Returns:
+    - The Precision @ k value (integer, must be in range [0, 1])
+    '''
+
+    count = 0
+
+    for idx in range(k):
+        if ranking[idx] in relevant_docs:
+            count += 1
+    
+    return count/k
+
+def avg_precision_k(ranking, relevant_docs, k):
+    '''
+    Computes the Average Precision @ k for a ranking given the indices relevant documents.
+
+    Attributes:
+    - ranking: a list of document indices (integers) order in decreasing predicted relevance, must not be empty
+    - relevant_docs: a list of document indices (integers) that are relevant, must not be empty
+
+    Returns:
+    - The AP @ k value (integer, must be in range [0, 1])
+    '''
+
+    count = 0
+    avg_p = 0
+
+    for idx in range(k):
+        if ranking[idx] in relevant_docs:
+            count += 1
+            avg_p += precision_k(ranking, relevant_docs, idx+1)
+
+    if not count:
+        return 0
+    return avg_p/count
+
+def mean_avg_precision_k(rankings, relevant_docs, k):
+    '''
+    Computes the Mean Average Precision @ k for a ranking given the indices relevant documents.
+
+    Attributes:
+    - rankings: a list of lists of document indices (integers) order in decreasing predicted relevance, must not be empty
+    - relevant_docs: a list of document indices (integers) that are relevant, must not be empty
+
+    Returns:
+    - The MAP @ k value (integer, must be in range [0, 1])
+    '''
+
+    num_queries = len(rankings)
+    assert len(relevant_docs) == num_queries, 'rankings and relevant_docs must be of the same length'
+
+    map = 0
+    for idx in range(num_queries):
+        map += avg_precision_k(rankings[idx], relevant_docs[idx], k)
+
+    map /= num_queries
+    return map
+
+def get_relevant_docs(sim_scores, num_docs):
+    '''
+    Given a similarity score matrix, where M[i, j] is the similarity between the i-th doc and j-th query, return a list of relevant docs.
+
+    Attributes:
+    - sim_scores: a matrix of dimensions (n x num_queries)
+    - num_docs: number of desired relevant documents
+
+    Returns:
+    - A list of lists of indices (integers) of the k_docs most similar documents for each query sorted from most to least similar.
+    '''
+    n, num_queries = sim_scores.shape
+
+    result = []
+
+    for q_idx in range(num_queries):
+        doc_axis, q_axis = np.nonzero(sim_scores.argsort(axis = 0) >= n-num_docs)
+        rel_docs = doc_axis[q_axis == q_idx].tolist()
+        rel_docs.sort(key = lambda doc: sim_scores[doc, q_idx], reverse = True)
+        result.append(rel_docs)
+
+    return result
 
 
 # tests
@@ -110,7 +200,105 @@ def test_mean_reciprocal_rank_k():
     assert mean_reciprocal_rank_k(rank3, docs3, k3) == 1, f'failed test 2'
     print('passed test 3')
 
+def test_get_relevant_docs():
+
+    print('testing get_relevant_docs:')
+
+    sim_scores1 = np.array([[0.1, 0.2, 0.3], [0.2, 0.4, 0.2], [0.3, 0.2, 0.1]])
+    k_docs1 = 1
+    assert get_relevant_docs(sim_scores1, k_docs1) == [[2], [1], [0]], 'failed test 1'
+    print('passed test 1')
+
+    sim_scores1 = np.array([[0.1, 0.2, 0.3], [0.2, 0.4, 0.2], [0.3, 0.3, 0.1]])
+    k_docs1 = 2
+    assert get_relevant_docs(sim_scores1, k_docs1) == [[2, 1], [1, 2], [0, 1]]
+    print('passed test 2')
+
+    sim_scores1 = np.array([[0.3], [0.2], [0.3]])
+    k_docs1 = 1
+    docs1 = get_relevant_docs(sim_scores1, k_docs1)[0]
+    assert 0 in docs1 or 2 in docs1, 'failed test 3'
+    print('passed test 3')
+
+
+def test_precision_k():
+    print('testing precision_k:')
+
+    rank1 = [1, 2, 4, 5]
+    docs1 = [4, 5]
+    k1 = 3
+    assert precision_k(rank1, docs1, k1) == 1/3, f'failed test 1'
+    print('passed test 1')
+
+    rank2 = [1, 2, 4, 5]
+    docs2 = [4, 5]
+    k2 = 2
+    assert precision_k(rank2, docs2, k2) == 0, f'failed test 2'
+    print('passed test 2')
+
+    rank3 = [4, 5, 1, 2]
+    docs3 = [4, 5, 3]
+    k3 = 2
+    assert precision_k(rank3, docs3, k3) == 1, f'failed test 3'
+    print('passed test 3')
+
+def test_avg_precision_k():
+    print('testing avg_precision_k:')
+
+    rank1 = [1, 2, 4, 5]
+    docs1 = [4, 5]
+    k1 = 3
+    assert avg_precision_k(rank1, docs1, k1) == 1/3, f'failed test 1'
+    print('passed test 1')
+
+    rank2 = [1, 2, 4, 5]
+    docs2 = [4, 5]
+    k2 = 2
+    assert avg_precision_k(rank2, docs2, k2) == 0, f'failed test 2'
+    print('passed test 2')
+
+    rank3 = [4, 5, 1, 2]
+    docs3 = [4, 5, 3]
+    k3 = 2
+    assert avg_precision_k(rank3, docs3, k3) == 1, f'failed test 3'
+    print('passed test 3')
+
+    rank4 = [4, 5, 1, 2]
+    docs4 = [4, 5, 3]
+    k4 = 3
+    assert avg_precision_k(rank4, docs4, k4) == 1, f'failed test 4'
+    print('passed test 4')
+
+def test_mean_avg_precision_k():
+    epsilon = 10**-6
+
+    print('testing mean_avg_precision_k:')
+
+    rank1 = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+    docs1 = [[4, 5], [2, 3]]
+    k1 = 3
+    assert abs(7/24-mean_avg_precision_k(rank1, docs1, k1)) < epsilon, f'failed test 1'
+    print('passed test 1')
+
+    rank2 = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]]
+    docs2 = [[4, 5], [1, 2]]
+    k2 = 2
+    assert mean_avg_precision_k(rank2, docs2, k2) == 0, f'failed test 2'
+    print('passed test 2')
+
+    rank3 = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]]
+    docs3 = [[1, 2], [4, 5]]
+    k3 = 2
+    assert mean_avg_precision_k(rank3, docs3, k3) == 1, f'failed test 2'
+    print('passed test 3')
+
+
+
 if __name__ == '__main__':
     test_reciprocal_rank()
     test_reciprocal_rank_k()
     test_mean_reciprocal_rank_k()
+    test_get_relevant_docs()
+    test_precision_k()
+    test_avg_precision_k()
+    test_mean_avg_precision_k()
